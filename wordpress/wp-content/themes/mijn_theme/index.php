c1ad188420cd876cb679d8be2b38345ff9ea222a
timo weel <weel.timo@gmail.com>
2023-07-03 14:47:13 +0200

    een basis

--- wordpress/wp-content/themes/mijn_theme/index.php
+++ wordpress/wp-content/themes/mijn_theme/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
     <meta charset="UTF-8">
@@ -10,7 +11,7 @@
 
     <!-- De onderstaande regel code gebruikt de WordPress-functie get_stylesheet_uri() om de URL van het huidige thema's style.css-bestand op te halen en dit te koppelen aan het stijlblad van je website. -->
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-    
+
 </head>
 
 <body>
@@ -22,25 +23,25 @@
 
             <!-- De onderstaande regel code gebruikt de WordPress-functie  -->
             <?php
-                 wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
+            wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
             ?>
         </nav>
     </header>
 
     <main>
-    <!-- De onderstaande regels code kijkt of en hoeveel posts er zijn en plaatst deze op de pagina.  -->
-    <?php
+        <!-- De onderstaande regels code kijkt of en hoeveel posts er zijn en plaatst deze op de pagina.  -->
+        <?php
         if (have_posts()) :
             while (have_posts()) :
                 the_post();
-                ?>
+        ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <div class="entry-content">
                         <?php the_content(); ?>
                     </div>
                 </article>
-                <?php
+            <?php
             endwhile;
         else :
             ?>
@@ -49,7 +50,8 @@
     </main>
 
     <footer>
-            <!-- Voeg hier je footerinhoud toe -->
+        <!-- Voeg hier je footerinhoud toe -->
     </footer>
 </body>
+
 </html>
